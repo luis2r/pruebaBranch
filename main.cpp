7c541cba688aff74f3dfd4591e7479627bd960f3
luis <luis@luis-Vostro-270s>
2015-05-13 17:06:18 -0300

    primer comit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: luis
+ *
+ * Created on May 13, 2015, 5:04 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
